e1e8aeb74deb0df96af830a81b266bf0ffe878d5
Aaron Bockover <abockover@novell.com>
2009-12-07 09:43:20 +0800

    [mirror_binaries] show total progress information

--- mirror_binaries.py
+++ mirror_binaries.py
@@ -9,9 +9,6 @@
 # either version 2, or (at your option) any later version
 #
 
-@cmdln.option ('-q', '--quiet', action='store_true',
-    help='Do not show downloading progress')
-
 @cmdln.option ('-d', '--destdir', metavar='DIR',
     help='Destination directory. Default is the package cache dir as '
          'defined in osc config or overridden by OSC_PACKAGECACHEDIR. '
@@ -43,12 +40,29 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
     else:
         print 'Using %s' % opts.destdir
 
+    finished_bytes = 0
+    finished_binaries = 0
+    total_binaries = len (binaries)
+    total_bytes = 0
     for binary in binaries:
+        total_bytes += binary.size
+
+    for binary in binaries:
+        finished_binaries += 1
+        finished_bytes += binary.size
+        print '==> %d / %d, %d MB / %d MB, %d%%'\
+            % (finished_binaries,
+               total_binaries,
+               finished_bytes / 1024 / 1024,
+               total_bytes / 1024 / 1024,
+               (float (finished_bytes) / float (total_bytes)) * 100.0)
+        print '    Checking for suitable %s' % binary.name
+
         # Look for existing RPMs with same name, modification time, and size.
         # We have to do this because OBS doesn't give us properly named RPM 
         # files for unpublished RPM binaries. After fetching the binary we will 
         # rename it properly so it can be picked up by osc build.
-        (package, ext) = os.path.splitext (binary.name)
+        package, ext = os.path.splitext (binary.name)
         skip_binary = None
         for existing in glob.glob (os.path.join (opts.destdir, package) + '*'):
             st = os.stat (existing)
@@ -56,20 +70,21 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
                 skip_binary = existing
                 break
         if skip_binary:
-            print 'Skipping %s' % os.path.basename (skip_binary)
+            print '    Skipping, found %s' % os.path.basename (skip_binary)
             continue
 
         # Download the binary
         target_filename = os.path.join (opts.destdir, binary.name)
+        print '    ',
         get_binary_file (apiurl, project, repository, architecture,
             binary.name,
             target_filename = target_filename,
             target_mtime = binary.mtime,
-            progress_meter = not opts.quiet)
+            progress_meter = True)
 
         # Update the filename based on RPM data
         new_target_filename = self.get_rpm_filename (target_filename)
-        print 'Renaming %s to %s' % \
+        print '    Renaming %s to %s' % \
             (binary.name, os.path.basename (new_target_filename))
         os.rename (target_filename, new_target_filename)
 
@@ -85,9 +100,9 @@ def get_rpm_filename (self, filename):
     arch = rpm_data['Arch:']
     if not rpm_data['SourceRPM:']:
         if rpm_data['NoSource:'] or rpm_data['NoPatch:']:
-            arch = "nosrc"
+            arch = 'nosrc'
         else:
-            arch = "src"
+            arch = 'src'
     
     canonname = '%s-%s-%s.%s.rpm' % (rpm_data['Name:'], 
         rpm_data['Version:'], rpm_data['Release:'], arch)
