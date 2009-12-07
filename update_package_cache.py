647d43ee551e5fb4d22cf12603705f6bcea89b63
Aaron Bockover <abockover@novell.com>
2009-12-08 06:34:58 +0800

    [update_package_cache] prune, existing, verbose
    Renamed mirror_binaries to update_package_cache since this is
    turning into more of a cache management tool.
    
    Added three new features:
    
     - prune: removes packages not identified to be the same name,
              size, and mtime as what OBS reports (e.g. older
              versions); it might be better/safer to parse all RPMs
              of the same package name and actually perform a real
              RPM package version comparison to identify prune
              candidates
    
     - existing: only download a package if an older version was
                 already in the cache; this is useful to help
                 update a cache that was already primed by osc build
    
     - verbose

--- update_package_cache.py
+++ update_package_cache.py
@@ -1,4 +1,4 @@
-# mirror_binaries.py
+# update_package_cache.py
 # osc plugin to create a link mirror of an OBS project
 #
 # Written by Aaron Bockover <abockover@novell.com>
@@ -15,7 +15,18 @@
          'if the package cache is used, additionaly directory structure '
          'will be created so the packages can be used for building.')
 
-def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
+@cmdln.option ('-e', '--existing', action='store_true',
+    help='Only download updated versions of packages that have '
+         'been previously downloaded.')
+
+@cmdln.option ('-p', '--prune', action='store_true',
+    help='Remove older versions of packages that have '
+         'been previously downloaded.')
+
+@cmdln.option ('-v', '--verbose', action='store_true',
+    help='Show extra progress and processing details.')
+
+def do_update_package_cache (self, subcmd, opts, project, repository, architecture):
     """${cmd_name}: Fetch all published binaries from a project
 
     ${cmd_usage}
@@ -25,6 +36,12 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
     import glob
 
     apiurl = conf.config['apiurl']
+
+    self.prune_stats = { 'files': [], 'bytes': 0 }
+
+    if opts.verbose:
+        print 'Fetching package list for %s/%s/%s/%s' % (apiurl, project, 
+            repository, architecture)
     binaries = get_binarylist (apiurl, project, 
         repository, architecture, verbose = True)
     if binaries == []:
@@ -35,10 +52,11 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
             project, repository, architecture)
 
     if not os.path.isdir (opts.destdir):
-        print 'Creating %s' % opts.destdir
+        if opts.verbose:
+            print 'Creating cache directory: %s' % opts.destdir
         os.makedirs (opts.destdir, 0755)
-    else:
-        print 'Using %s' % opts.destdir
+    elif opts.verbose:
+        print 'Using cache directory: %s' % opts.destdir
 
     finished_bytes = 0
     finished_binaries = 0
@@ -50,32 +68,48 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
     for binary in binaries:
         finished_binaries += 1
         finished_bytes += binary.size
-        print '==> %d / %d, %d MB / %d MB, %d%%'\
-            % (finished_binaries,
-               total_binaries,
-               finished_bytes / 1024 / 1024,
-               total_bytes / 1024 / 1024,
-               (float (finished_bytes) / float (total_bytes)) * 100.0)
-        print '    Checking for suitable %s' % binary.name
+        if opts.verbose:
+            print '==> %d / %d, %d MB / %d MB, %d%%'\
+                % (finished_binaries,
+                   total_binaries,
+                   finished_bytes / 1024 / 1024,
+                   total_bytes / 1024 / 1024,
+                   (float (finished_bytes) / float (total_bytes)) * 100.0)
+            print '    Checking for suitable %s' % binary.name
 
         # Look for existing RPMs with same name, modification time, and size.
         # We have to do this because OBS doesn't give us properly named RPM 
         # files for unpublished RPM binaries. After fetching the binary we will 
         # rename it properly so it can be picked up by osc build.
         package, ext = os.path.splitext (binary.name)
+        local_binaries = glob.glob (os.path.join (opts.destdir, package) + '*')
         skip_binary = None
-        for existing in glob.glob (os.path.join (opts.destdir, package) + '*'):
+
+        for existing in local_binaries:
             st = os.stat (existing)
             if st.st_mtime == binary.mtime and st.st_size == binary.size:
-                skip_binary = existing
-                break
+                if self.get_rpm_package_name (existing) == package:
+                    skip_binary = existing
+                    break
+
         if skip_binary:
-            print '    Skipping, found %s' % os.path.basename (skip_binary)
+            if opts.verbose:
+                print '    Skipping, found %s' % os.path.basename (skip_binary)
+            if opts.prune:
+                self.prune (package, local_binaries, skip_binary, opts.verbose)
+            continue
+        elif opts.existing and local_binaries == []:
+            if opts.verbose:
+                print '    Skipping, no older version found locally'
             continue
 
         # Download the binary
         target_filename = os.path.join (opts.destdir, binary.name)
-        print '    ',
+        if opts.verbose:
+            print '    ',
+        else:
+            print '%d/%d %dMB: ' % (finished_binaries, total_binaries,
+                binary.size / 1024 / 1024),
         get_binary_file (apiurl, project, repository, architecture,
             binary.name,
             target_filename = target_filename,
@@ -84,9 +118,30 @@ def do_mirror_binaries (self, subcmd, opts, project, repository, architecture):
 
         # Update the filename based on RPM data
         new_target_filename = self.get_rpm_filename (target_filename)
-        print '    Renaming %s to %s' % \
-            (binary.name, os.path.basename (new_target_filename))
+        if opts.verbose:
+            print '    Renaming %s to %s' % \
+                (binary.name, os.path.basename (new_target_filename))
         os.rename (target_filename, new_target_filename)
+
+        if opts.prune and not local_binaries == []:
+            self.prune (package, local_binaries, new_target_filename, opts.verbose)
+
+    if not self.prune_stats['files'] == []:
+        print '%d obsolete RPMs removed (%d KB)' % (len (self.prune_stats['files']),
+            self.prune_stats['bytes'])
+
+
+def prune (self, package_name, local_binaries, keep_binary, verbose):
+    for binary in local_binaries[:]:
+        if not binary == keep_binary and self.get_rpm_package_name (binary) == package_name:
+            self.prune_stats['files'].append (binary)
+            self.prune_stats['bytes'] += os.stat (binary).st_size
+            if opts.verbose:
+                print '    Removed obsolete %s' % os.path.basename (binary)
+
+
+def get_rpm_package_name (self, filename):
+    return data_from_rpm (filename, 'Name:')['Name:']
 
 
 def get_rpm_filename (self, filename):
